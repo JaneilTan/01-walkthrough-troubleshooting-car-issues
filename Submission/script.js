a2cb36a91638c2be5eaef1d20c59483ca7dcb99f
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-03-11 14:25:38 +1300

    Solve fourth test scenario

--- Submission/script.js
+++ Submission/script.js
@@ -4,21 +4,44 @@
  * @returns {string} The reply which is either the next question or answer. Example "Are the battery terminals corroded?"
  */
 let level = 1;
+let path;
 
 const getReply = (userInput) => {
   if (level === 1) {
     level = 2;
+    if (userInput === "yes") {
+      path = "yes";
     return "Are the battery terminals corroded?";
+    }
+    if (userInput === "no") {
+      path = "no";
+    return "Does the car make a clicking noise?";
+    }
   }
   
   if (level === 2) {
-    if (userInput === "yes") {
-    return "Clean terminals and try starting again";
+    level = 3;
+    if (path === "yes") {
+      if (userInput === "yes") {
+      return "Clean terminals and try starting again";
+    }
+
+      if (userInput === "no") {
+        return "Replace cables and try again";
+      }
+    }
+    if (path === "no") {
+      if (userInput === "yes") {
+      return "Replace the battery";
+    }
+      if (userInput === "no") {
+        return "Does the car crank up but fail to start?";
+      }
+    }
   }
 
-    if (userInput === "no") {
-      return "Replace cables and try again";
-    }
+  if (level === 3) {
+    return "Check spark plug connections";
   }
 };
 
--- test/script.test.js
+++ test/script.test.js
@@ -26,20 +26,60 @@ describe("getReply", () => {
     // Uncomment the following line and update your actual reply with your expected reply
     expect(reply1).toEqual(expectedReply1);
   });
+});
 
   it("should reply with the correct response for: yes, no", () => {
     // Input: "yes"
     const reply1 = getReply("yes");
     // Output: "Are the battery terminals corroded?"
     const expectedReply1 = "Are the battery terminals corroded?";
+
+    expect(reply1).toEqual(expectedReply1);
+    
     // Input: "yes"
     const reply2 = getReply("no");
     // Output: "Replace cables and try again"
     const expectedReply2 = "Replace cables and try again";
-
     
-
     // Uncomment the following line and update your actual reply with your expected reply
-    expect(reply1).toEqual(expectedReply1);
+    expect(reply2).toEqual(expectedReply2);
   });
+
+  it("should reply with the correct response for: yes, no", () => { 
+  // Input: "no"
+  const reply1 = getReply("no");
+  // Output: "Does the car make a clicking noise?"
+  const expectedReply1 = "Does the car make a clicking noise?";
+
+  expect(reply1).toEqual(expectedReply1);
+
+  // Input: "yes"
+  const reply2 = getReply("yes");
+  // Output: "Replace the battery"
+  const expectedReply2= "Replace the battery";
+
+  expect(reply2).toEqual(expectedReply2);
+});
+
+it("should reply with the correct response for: no, no, yes", () => { 
+// Input: "no"
+const reply1 = getReply("no");
+// Output: "Does the car make a clicking noise?"
+const expectedReply1 = "Does the car make a clicking noise?";
+
+expect(reply1).toEqual(expectedReply1);
+
+// Input: "no"
+const reply2 = getReply("no");
+// Output: "Does the car crank up but fail to start?"
+const expectedReply2 = "Does the car crank up but fail to start?";
+
+expect(reply2).toEqual(expectedReply2);
+
+// Input "yes"
+const reply3 = getReply("yes");
+// Output: "Check spark plug connections"
+const expectedReply3 = "Check spark plug connections";
+
+expect(reply3).toEqual(expectedReply3);
 });
